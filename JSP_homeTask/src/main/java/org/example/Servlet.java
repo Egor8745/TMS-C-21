5c9c35e7c34fd40d376bb34633ce5035b96ea117
Егор <sedunegor123@gmail.com>
2023-08-17 20:22:56 +0300

    Revert "Delete JSP_homeTask directory"
    This reverts commit c37af90c42e6df2060e8b0fed8ae898b0b6b26db.

---
+++ JSP_homeTask/src/main/java/org/example/Servlet.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("title");
+        String password = req.getParameter("author");
+        if (login == null || login.isEmpty() || password == null || password.isEmpty()) {
+            req.getRequestDispatcher("save-request.jsp").forward(req, resp);
+        }
+        req.getRequestDispatcher("applicationAccepted.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("save-request.jsp").forward(req, resp);
+    }
+}
+
+
